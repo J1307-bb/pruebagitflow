dc8216adde441fd3e62d3ce95dc671db2f1390be
Jaime <jaimecastaneda133@gmail.com>
2024-06-12 18:27:36 -0500

    Add: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Formulario from './components/Formulario'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
+      <Header />
       <Formulario />
     </>
   )
